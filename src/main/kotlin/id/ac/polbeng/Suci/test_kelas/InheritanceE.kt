a06048478db06aa4525344775f78eade55cfd75b
suciifrndzz <sucizulianafernandes298@gmail.com>
2024-09-28 21:29:41 +0700

    Update InheritanceE.kt

--- src/main/kotlin/id/ac/polbeng/Suci/test_kelas/InheritanceE.kt
+++ src/main/kotlin/id/ac/polbeng/Suci/test_kelas/InheritanceE.kt
@@ -3,17 +3,25 @@ package id.ac.polbeng.Suci.test_kelas
 open class NewPerson {
     open var age: Int = 1
 }
-class CheckedPerson: NewPerson() {
+
+class CheckedPerson : NewPerson() {
     override var age: Int = 1
         set(value) {
-           field = if(value > 0) value else throw IllegalArgumentException("Age can not be negative")
+            field = if (value > 0) value else throw IllegalArgumentException("Age cannot be negative")
         }
 }
+
 fun main() {
     val person = NewPerson()
-    person.age = -5//works
+    person.age = -5 // works, but not recommended
 
     val checkedPerson = CheckedPerson()
-    checkedPerson.age = -5
-// Throws IllegalArgumentException : Age can not be negative
+
+    // Pastikan untuk memeriksa nilai sebelum mengatur
+    val newAge = -5
+    if (newAge > 0) {
+        checkedPerson.age = newAge
+    } else {
+        println("Invalid age: $newAge. Age cannot be negative.")
+    }
 }
